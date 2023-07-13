b6176b9c2b7f26a656ec99950d8679cdb717c9d3
sekigmai369 <skgmyum@gmail.com>
2023-07-13 21:52:50 +0900

    Initial commit

---
+++ Aoj9-B.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class P
+{
+	public static void Main(string[] args)
+	{
+		while (true)
+		{
+            string cards = Console.ReadLine();
+            if (cards == "-")
+			{
+				break;
+			}
+			int m = int.Parse(Console.ReadLine());
+			for(int i = 0; i < m; i++)
+			{
+				int h = int.Parse(Console.ReadLine());
+				cards = cards.Substring(h, cards.Length - h) + cards.Substring(0, h); 
+			}
+			Console.WriteLine(cards);
+		}
+		Console.WriteLine();
+	}
+}
